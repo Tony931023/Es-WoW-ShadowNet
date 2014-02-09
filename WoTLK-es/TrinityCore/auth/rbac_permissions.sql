8b91a2b20261416ef5831399f5593378df2a6e1c
tony931023 <tony931023@gmail.com>
2014-02-09 01:28:11 -0500

    Traduccion de auth.rbc_permissions

---
+++ WoTLK-es/TrinityCore/auth/rbac_permissions.sql
@@ -0,0 +1,6 @@
+-- ========================== --
+--      rbac_permissions      --
+-- ========================== -- 
+
+UPDATE `auth`.`rbac_permissions` SET `name` = 'Desconexión Instantánea' WHERE `rbac_permissions`.`id` =1;
+
