3db74135cb3a68f6725c770ba350179fff96d36b
tony931023 <tony931023@gmail.com>
2014-02-09 01:33:21 -0500

    Quitar la base de datos auth, por si el usuario utiliza otro nombre para la base de datos

--- WoTLK-es/TrinityCore/auth/rbac_permissions.sql
+++ WoTLK-es/TrinityCore/auth/rbac_permissions.sql
@@ -2,5 +2,5 @@
 --      rbac_permissions      --
 -- ========================== -- 
 
-UPDATE `auth`.`rbac_permissions` SET `name` = 'Desconexión Instantánea' WHERE `rbac_permissions`.`id` =1;
+UPDATE `rbac_permissions` SET `name` = 'Desconexión Instantánea' WHERE `rbac_permissions`.`id` =1;
 
