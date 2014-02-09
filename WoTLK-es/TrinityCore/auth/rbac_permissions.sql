9594665bfe4e8c068be20d6706192a89468c2196
tony931023 <tony931023@gmail.com>
2014-02-09 03:28:35 -0500

    Agregar Traduccion para los ID 2,3,4,5,6,11,13 al 19

--- WoTLK-es/TrinityCore/auth/rbac_permissions.sql
+++ WoTLK-es/TrinityCore/auth/rbac_permissions.sql
@@ -3,4 +3,16 @@
 -- ========================== -- 
 
 UPDATE `rbac_permissions` SET `name` = 'Desconexión Instantánea' WHERE `rbac_permissions`.`id` =1;
-
+UPDATE `rbac_permissions` SET `name` = 'Saltar Cola' WHERE `rbac_permissions`.`id` =2;
+UPDATE `rbac_permissions` SET `name` = 'Unirce al Campo de Batalla Normal' WHERE `rbac_permissions`.`id` =3;
+UPDATE `rbac_permissions` SET `name` = 'Unirce al Campo de Batalla Aleatorio' WHERE `rbac_permissions`.`id` =4;
+UPDATE `rbac_permissions` SET `name` = 'Unirce a la Arena' WHERE `rbac_permissions`.`id` =5;
+UPDATE `rbac_permissions` SET `name` = 'Unirce al Buscador de Masmorras' WHERE `rbac_permissions`.`id` =6;
+UPDATE `rbac_permissions` SET `name` = 'Registro de Operaciónes del GM' WHERE `rbac_permissions`.`id` =11;
+UPDATE `rbac_permissions` SET `name` = 'Saltar el Chequeo de Requerido de Boses' WHERE `rbac_permissions`.`id` =13;
+UPDATE `rbac_permissions` SET `name` = 'Saltar el Chequeo de Máscara de Creación del Equipo de Personaje' WHERE `rbac_permissions`.`id` =14;
+UPDATE `rbac_permissions` SET `name` = 'Saltar el Chequeo de Máscara de Creación del Clase de Personaje' WHERE `rbac_permissions`.`id` =15;
+UPDATE `rbac_permissions` SET `name` = 'Saltar el Chequeo de Máscara de Creación del Raza de Personaje' WHERE `rbac_permissions`.`id` =16;
+UPDATE `rbac_permissions` SET `name` = 'Saltar el Chequeo de Nombre Reservado del Personaje' WHERE `rbac_permissions`.`id` =17;
+UPDATE `rbac_permissions` SET `name` = 'Saltar la Creación Mínimo Nivel del Personaje Heroico' WHERE `rbac_permissions`.`id` =18;
+UPDATE `rbac_permissions` SET `name` = 'Saltar la Comprobación de los Requisitos Necesarios para Usar Canales' WHERE `rbac_permissions`.`id` =18;
